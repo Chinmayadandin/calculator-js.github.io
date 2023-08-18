540f8430beb36b4244d5c391d7a8ff577fdb1513
Chinmayadandin <114335708+Chinmayadandin@users.noreply.github.com>
2023-08-18 20:46:06 +0530

    Add files via upload
    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("Hello World!");
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+function deleteLast() {
+  let result = document.getElementById("result");
+  result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+  let result = document.getElementById("result");
+  result.value = eval(result.value);
+}
+
+function appendToResult(val) {
+  let result = document.getElementById("result");
+  result.value += val;
+}
